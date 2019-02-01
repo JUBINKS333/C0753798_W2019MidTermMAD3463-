9dad2ffe1c31a04a6a109224265ea7df4e77bac1
JubinKS333 <jubinks3@gmail.com>
2019-01-31 20:27:04 -0500

    6

--- src/com/lambton/magicalcardgame/MagicalCardGameModelInterface.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameModelInterface.java
@@ -1,4 +1,9 @@
 package com.lambton.magicalcardgame;
 
 public class MagicalCardGameModelInterface {
+    public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel);
+
+    public String[][] getSecShuffleResult(MagicalCardGameModel magicalCardGameModel);
+
+    public String getFinalResult(MagicalCardGameModel magicalCardGameModel);
 }
