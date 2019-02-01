4c4c5b2b53392e3f82d7d7baba2d05ba5a9758e5
JubinKS333 <jubinks3@gmail.com>
2019-01-31 20:20:50 -0500

    second

---
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -0,0 +1,17 @@
+package com.lambton.magicalcardgame;
+
+public class MagicalCardGameCalculator {
+    public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel) {
+        return String[][];
+    }
+        public String[][] getSecShuffleResult (MagicalCardGameModel magicalCardGameModel)
+        {
+            return String[][];
+        }
+
+
+    public String getFinalResult(MagicalCardGameModel magicalCardGameModel)
+    {
+        return null;
+    }
+}
